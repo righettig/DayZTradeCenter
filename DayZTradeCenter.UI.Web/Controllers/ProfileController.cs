74e491a54b4ad763ef5ed46c6c01473c120604b1
righettig <righetti.giacomo@live.com>
2014-09-19 16:23:13 +0200

    Added profile controller.

---
+++ DayZTradeCenter.UI.Web/Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace DayZTradeCenter.UI.Web.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
