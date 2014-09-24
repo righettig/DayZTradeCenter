1994de8498e194d0737c2760e8b70348eaa85675
righettig <righetti.giacomo@live.com>
2014-09-24 23:07:23 +0200

    Landing/home page: added latest trade details.

--- DayZTradeCenter.UI.Web/Controllers/HomeController.cs
+++ DayZTradeCenter.UI.Web/Controllers/HomeController.cs
@@ -1,13 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using DayZTradeCenter.DomainModel;
+using rg.GenericRepository.Core;
 
 namespace DayZTradeCenter.UI.Web.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="tradesRepository">The trades repository.</param>
+        /// <exception cref="System.ArgumentNullException">tradesRepository</exception>
+        public HomeController(IRepository<Trade> tradesRepository)
+        {
+            if (tradesRepository == null)
+            {
+                throw new ArgumentNullException("tradesRepository");
+            }
+
+            _tradesRepository = tradesRepository;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var latestTrade =
+                _tradesRepository
+                    .GetAll()
+                    .OrderByDescending(trade => trade.CreationDate)
+                    .FirstOrDefault();
+
+            return View(latestTrade);
         }
+
+        private readonly IRepository<Trade> _tradesRepository;
     }
 }
