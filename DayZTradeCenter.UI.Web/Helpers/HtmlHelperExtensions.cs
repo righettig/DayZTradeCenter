9430b2e777a1e42de222892d9afa56cac7e3cf1c
righettig <righetti.giacomo@live.com>
2014-10-09 14:18:22 +0200

    Added build version in the footer section.

---
+++ DayZTradeCenter.UI.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DayZTradeCenter.UI.Web.Helpers
+{
+    // http://stackoverflow.com/questions/12694267/print-version-number-in-asp-net-mvc-4-app
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString AssemblyVersion(this HtmlHelper helper)
+        {
+            var version = 
+                Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            return MvcHtmlString.Create(version);
+        }
+    }
+}
