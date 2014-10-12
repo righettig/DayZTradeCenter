bd279e4a7aba48f0673762747e4f8489390332c4
righettig <righetti.giacomo@live.com>
2014-10-12 15:01:43 +0200

    Admin, Items.Create, added missing fields (rarity, category, subcategory).

--- DayZTradeCenter.UI.Web/Controllers/ItemsController.cs
+++ DayZTradeCenter.UI.Web/Controllers/ItemsController.cs
@@ -48,7 +48,7 @@ namespace DayZTradeCenter.UI.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id, Name")] Item item)
+        public ActionResult Create([Bind(Include = "Id, Name, Rarity, Details")] Item item)
         {
             if (!ModelState.IsValid)
             {
