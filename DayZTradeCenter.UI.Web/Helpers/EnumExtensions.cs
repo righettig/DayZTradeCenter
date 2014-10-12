da9b32cb25c97018b2ce15ff63ad59195505ff50
righettig <righetti.giacomo@live.com>
2014-10-12 14:56:53 +0200

    Admin, Item.Details, enum properties are now displayed with localized strings.

--- DayZTradeCenter.UI.Web/Helpers/EnumExtensions.cs
+++ DayZTradeCenter.UI.Web/Helpers/EnumExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
@@ -9,22 +10,38 @@ namespace DayZTradeCenter.UI.Web.Helpers
 {
     public static class EnumExtensions
     {
-        public static MvcHtmlString LocalizedEnumDropDownListFor<TModel, TProperty>(
+        public static MvcHtmlString LocalizedEnumDropDownListFor<TModel, TEnum>(
             this HtmlHelper<TModel> htmlHelper,
-            Expression<Func<TModel, TProperty>> expression,
-            object htmlAttributes)
+            Expression<Func<TModel, TEnum>> expression,
+            object htmlAttributes) where TEnum : struct
         {
-            var values = Enum.GetValues(typeof(TProperty))
-                .Cast<Enum>(); // NB: 'Enum' instead of TProperty so that the ext method 'GetDescription' can be invoked.
+            var values = Values<TEnum>();
 
             var items = from value in values
-                        select new SelectListItem
-                        {
-                            Text = value.GetDescription(), // generalization of 'ToString'
-                            Value = value.ToString()
-                        };
+                select new SelectListItem
+                {
+                    Text = value.GetDescription(), // generalization of 'ToString'
+                    Value = value.ToString()
+                };
 
             return htmlHelper.DropDownListFor(expression, items, htmlAttributes);
+        }
+
+        public static MvcHtmlString LocalizedEnumDisplayFor<TModel, TEnum>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TEnum>> expression) where TEnum : struct
+        {
+            var values = Values<TEnum>();
+
+            var result = values
+                .First(
+                    x =>
+                        x.ToString() == ModelMetadata
+                            .FromLambdaExpression(expression, htmlHelper.ViewData)
+                            .SimpleDisplayText)
+                .GetDescription(); // generalization of 'ToString'
+
+            return new MvcHtmlString(result);
         }
 
         public static string GetDescription(this Enum enumValue)
@@ -34,13 +51,31 @@ namespace DayZTradeCenter.UI.Web.Helpers
                 .GetField(enumValue.ToString());
 
             var attributes =
-                (DescriptionAttribute[])fi.GetCustomAttributes(
-                    typeof(DescriptionAttribute),
+                (DescriptionAttribute[]) fi.GetCustomAttributes(
+                    typeof (DescriptionAttribute),
                     false);
 
             return attributes.Length > 0
                 ? attributes[0].Description
                 : enumValue.ToString();
+        }
+
+        /// <summary>
+        /// Gets all the values for the specified enum.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of the property.</typeparam>
+        /// <returns>The values for the specified enum.</returns>
+        /// <exception cref="System.ArgumentException">T must be an enumerated type.</exception>
+        private static IEnumerable<Enum> Values<TEnum>() where TEnum : struct
+        {
+            if (!typeof (TEnum).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type.");
+            }
+
+            return Enum // NB: 'Enum' instead of TEnum so that the ext method 'GetDescription' can be invoked.
+                .GetValues(typeof (TEnum))
+                .Cast<Enum>();
         }
     }
 }
