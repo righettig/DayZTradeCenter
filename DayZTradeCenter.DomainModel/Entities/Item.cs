bbb04009d0e9b53f02e452f14c2c47d52ebee656
righettig <righetti.giacomo@live.com>
2014-10-12 13:31:07 +0200

    Added project for strings localization. Admin, Item.Edit, rarity, category and subcategory are now using the localized strings (= added whitespaces).

--- DayZTradeCenter.DomainModel/Entities/Item.cs
+++ DayZTradeCenter.DomainModel/Entities/Item.cs
@@ -1,4 +1,8 @@
-﻿using rg.GenericRepository.Core;
+﻿using System;
+using System.ComponentModel;
+using System.Resources;
+using DayZTradeCenter.Resources;
+using rg.GenericRepository.Core;
 
 namespace DayZTradeCenter.DomainModel.Entities
 {
@@ -42,103 +46,295 @@ namespace DayZTradeCenter.DomainModel.Entities
 
     public enum ItemCategories
     {
+        [LocalizedDescription("ItemCategories_Weapons", typeof(LocalizedStrings))]
         Weapons,
+
+        [LocalizedDescription("ItemCategories_Medical", typeof(LocalizedStrings))]
         Medical,
+
+        [LocalizedDescription("ItemCategories_Attachments", typeof(LocalizedStrings))]
         Attachments,
+
+        [LocalizedDescription("ItemCategories_Ammunition", typeof(LocalizedStrings))]
         Ammunition,
+
+        [LocalizedDescription("ItemCategories_Magazines", typeof(LocalizedStrings))]
         Magazines,
+
+        [LocalizedDescription("ItemCategories_FoodAndDrink", typeof(LocalizedStrings))]
         FoodAndDrink,
+
+        [LocalizedDescription("ItemCategories_Equipment", typeof(LocalizedStrings))]
         Equipment,
+
+        [LocalizedDescription("ItemCategories_Clothing", typeof(LocalizedStrings))]
         Clothing
     }
 
     public enum ItemSubcategories
     {
-        // Weapons
+        #region Weapons
+
+        [LocalizedDescription("ItemSubcategories_BluntMelee", typeof (LocalizedStrings))]
         BluntMelee,
+
+        [LocalizedDescription("ItemSubcategories_BladedMelee", typeof (LocalizedStrings))] 
         BladedMelee,
+
+        [LocalizedDescription("ItemSubcategories_ElectricWeapons", typeof (LocalizedStrings))] 
         ElectricWeapons,
+
+        [LocalizedDescription("ItemSubcategories_Bows", typeof (LocalizedStrings))] 
         Bows,
+
+        [LocalizedDescription("ItemSubcategories_Handguns", typeof (LocalizedStrings))] 
         Handguns,
+
+        [LocalizedDescription("ItemSubcategories_Rifles", typeof (LocalizedStrings))] 
         Rifles,
+
+        [LocalizedDescription("ItemSubcategories_SubmachineGuns", typeof (LocalizedStrings))] 
         SubmachineGuns,
+
+        [LocalizedDescription("ItemSubcategories_Shotguns", typeof (LocalizedStrings))] 
         Shotguns,
+
+        [LocalizedDescription("ItemSubcategories_Grenades", typeof (LocalizedStrings))] 
         Grenades,
 
-        // Medical
+        #endregion
+
+        #region Medical
+
+        [LocalizedDescription("ItemSubcategories_Bandages", typeof (LocalizedStrings))]
         Bandages,
+
+        [LocalizedDescription("ItemSubcategories_Splints", typeof (LocalizedStrings))] 
         Splints,
+
+        [LocalizedDescription("ItemSubcategories_BloodTransfusion", typeof (LocalizedStrings))] 
         BloodTransfusion,
+
+        [LocalizedDescription("ItemSubcategories_BloodTesting", typeof (LocalizedStrings))] 
         BloodTesting,
+
+        [LocalizedDescription("ItemSubcategories_Disinfection", typeof (LocalizedStrings))] 
         Disinfection,
+
+        [LocalizedDescription("ItemSubcategories_Medicines", typeof (LocalizedStrings))] 
         Medicines,
+
+        [LocalizedDescription("ItemSubcategories_Reanimation", typeof (LocalizedStrings))] 
         Reanimation,
+
+        [LocalizedDescription("ItemSubcategories_Temperature", typeof (LocalizedStrings))] 
         Temperature,
 
-        // Attachments
+        #endregion
+
+        #region Attachments
+
+        [LocalizedDescription("ItemSubcategories_Buttstocks", typeof(LocalizedStrings))]
         Buttstocks,
+
+        [LocalizedDescription("ItemSubcategories_Handguards", typeof(LocalizedStrings))]
         Handguards,
+
+        [LocalizedDescription("ItemSubcategories_SightsAndOptics", typeof(LocalizedStrings))]
         SightsAndOptics,
+
+        [LocalizedDescription("ItemSubcategories_Illumination", typeof(LocalizedStrings))]
         Illumination,
+
+        [LocalizedDescription("ItemSubcategories_MuzzleAttachments", typeof(LocalizedStrings))]
         MuzzleAttachments,
+
+        [LocalizedDescription("ItemSubcategories_Bayonets", typeof(LocalizedStrings))]
         Bayonets,
+
+        [LocalizedDescription("ItemSubcategories_Bipods", typeof(LocalizedStrings))]
         Bipods,
+
+        [LocalizedDescription("ItemSubcategories_Wraps", typeof(LocalizedStrings))]
         Wraps,
 
-        // Ammunition
+        #endregion
+
+        #region Ammunition
+
+        [LocalizedDescription("ItemSubcategories_HandgunAmmunition", typeof(LocalizedStrings))]
         HandgunAmmunition,
+
+        [LocalizedDescription("ItemSubcategories_ShotgunAmmunition", typeof(LocalizedStrings))]
         ShotgunAmmunition,
+
+        [LocalizedDescription("ItemSubcategories_RifleAmmunition", typeof(LocalizedStrings))]
         RifleAmmunition,
+
+        [LocalizedDescription("ItemSubcategories_MiscellaneousAmmunition", typeof(LocalizedStrings))]
         MiscellaneousAmmunition,
 
-        // Magazines
+        #endregion
+
+        #region Magazines
+
+        [LocalizedDescription("ItemSubcategories_HandgunMagazines", typeof(LocalizedStrings))]
         HandgunMagazines,
+
+        [LocalizedDescription("ItemSubcategories_RifleMagazines", typeof(LocalizedStrings))]
         RifleMagazines,
+
+        [LocalizedDescription("ItemSubcategories_SubMachineGunMagazines", typeof(LocalizedStrings))]
         SubMachineGunMagazines,
+
+        [LocalizedDescription("ItemSubcategories_Speedloaders", typeof(LocalizedStrings))]
         Speedloaders,
+
+        [LocalizedDescription("ItemSubcategories_MiscellaneousMagazines", typeof(LocalizedStrings))]
         MiscellaneousMagazines,
 
-        // Food and Drink
+        #endregion
+        
+        #region Food and Drink
+
+        [LocalizedDescription("ItemSubcategories_CannedGoods", typeof(LocalizedStrings))]
         CannedGoods,
+
+        [LocalizedDescription("ItemSubcategories_DryFood", typeof(LocalizedStrings))]
         DryFood,
+
+        [LocalizedDescription("ItemSubcategories_Fruits", typeof(LocalizedStrings))]
         Fruits,
+
+        [LocalizedDescription("ItemSubcategories_Vegetables", typeof(LocalizedStrings))]
         Vegetables,
+
+        [LocalizedDescription("ItemSubcategories_Berries", typeof(LocalizedStrings))]
         Berries,
+
+        [LocalizedDescription("ItemSubcategories_Drinks", typeof(LocalizedStrings))]
         Drinks,
 
-        // Equipment
+        #endregion
+
+        #region Equipment
+
+        [LocalizedDescription("ItemSubcategories_Backpacks", typeof(LocalizedStrings))]
         Backpacks,
+
+        [LocalizedDescription("ItemSubcategories_Containers", typeof(LocalizedStrings))]
         Containers,
+
+        [LocalizedDescription("ItemSubcategories_Cooking", typeof(LocalizedStrings))]
         Cooking,
+
+        [LocalizedDescription("ItemSubcategories_Decorational", typeof(LocalizedStrings))]
         Decorational,
+
+        [LocalizedDescription("ItemSubcategories_Devices", typeof(LocalizedStrings))]
         Devices,
+
+        [LocalizedDescription("ItemSubcategories_LightSources", typeof(LocalizedStrings))]
         LightSources,
+
+        [LocalizedDescription("ItemSubcategories_RepairKits", typeof(LocalizedStrings))]
         RepairKits,
+
+        [LocalizedDescription("ItemSubcategories_Resources", typeof(LocalizedStrings))]
         Resources,
+
+        [LocalizedDescription("ItemSubcategories_Tents", typeof(LocalizedStrings))]
         Tents,
+
+        [LocalizedDescription("ItemSubcategories_Tools", typeof(LocalizedStrings))]
         Tools,
+
+        [LocalizedDescription("ItemSubcategories_Other", typeof(LocalizedStrings))]
         Other,
 
-        // Clothing
+        #endregion
+
+        #region Clothing
+
+        [LocalizedDescription("ItemSubcategories_Eyewear", typeof(LocalizedStrings))]
         Eyewear,
+
+        [LocalizedDescription("ItemSubcategories_Hats", typeof(LocalizedStrings))]
         Hats,
+
+        [LocalizedDescription("ItemSubcategories_Helmets", typeof(LocalizedStrings))]
         Helmets,
+
+        [LocalizedDescription("ItemSubcategories_Masks", typeof(LocalizedStrings))]
         Masks,
+
+        [LocalizedDescription("ItemSubcategories_HandsAndArms", typeof(LocalizedStrings))]
         HandsAndArms,
+
+        [LocalizedDescription("ItemSubcategories_ShirtsAndJackets", typeof(LocalizedStrings))]
         ShirtsAndJackets,
+
+        [LocalizedDescription("ItemSubcategories_Holsters", typeof(LocalizedStrings))]
         Holsters,
+
+        [LocalizedDescription("ItemSubcategories_Vests", typeof(LocalizedStrings))]
         Vests,
+
+        [LocalizedDescription("ItemSubcategories_Pants", typeof(LocalizedStrings))]
         Pants,
+
+        [LocalizedDescription("ItemSubcategories_Boots", typeof(LocalizedStrings))]
         Boots
+
+        #endregion
     }
 
     public enum Rarities
     {
+        [LocalizedDescription("Rarities_Unknown", typeof(LocalizedStrings))]
         Unknown,
+
+        [LocalizedDescription("Rarities_NA", typeof(LocalizedStrings))]
         NA,
+
+        [LocalizedDescription("Rarities_Common", typeof(LocalizedStrings))]
         Common,
-        Uncommon, 
+
+        [LocalizedDescription("Rarities_Uncommon", typeof(LocalizedStrings))]
+        Uncommon,
+
+        [LocalizedDescription("Rarities_Rare", typeof(LocalizedStrings))]
         Rare,
+
+        [LocalizedDescription("Rarities_VeryRare", typeof(LocalizedStrings))]
         VeryRare
+    }
+
+    /// <summary>
+    /// To use localization for enum values.
+    /// <see cref="http://stackoverflow.com/questions/17380900/enum-localization"/>
+    /// </summary>
+    internal class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        private readonly string _resourceKey;
+        private readonly ResourceManager _resource;
+
+        public LocalizedDescriptionAttribute(string resourceKey, Type resourceType)
+        {
+            _resource = new ResourceManager(resourceType);
+            _resourceKey = resourceKey;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                var displayName = _resource.GetString(_resourceKey);
+
+                return string.IsNullOrEmpty(displayName)
+                    ? string.Format("[[{0}]]", _resourceKey)
+                    : displayName;
+            }
+        }
     }
 }
---
+++ DayZTradeCenter.UI.Web/Helpers/EnumExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace DayZTradeCenter.UI.Web.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static MvcHtmlString LocalizedEnumDropDownListFor<TModel, TProperty>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression,
+            object htmlAttributes)
+        {
+            var values = Enum.GetValues(typeof(TProperty))
+                .Cast<Enum>(); // NB: 'Enum' instead of TProperty so that the ext method 'GetDescription' can be invoked.
+
+            var items = from value in values
+                        select new SelectListItem
+                        {
+                            Text = value.GetDescription(), // generalization of 'ToString'
+                            Value = value.ToString()
+                        };
+
+            return htmlHelper.DropDownListFor(expression, items, htmlAttributes);
+        }
+
+        public static string GetDescription(this Enum enumValue)
+        {
+            var fi = enumValue
+                .GetType()
+                .GetField(enumValue.ToString());
+
+            var attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                    typeof(DescriptionAttribute),
+                    false);
+
+            return attributes.Length > 0
+                ? attributes[0].Description
+                : enumValue.ToString();
+        }
+    }
+}
