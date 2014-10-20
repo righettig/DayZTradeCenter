d647812ffbc625b77722c861c64b5d9505c5d561
righettig <righetti.giacomo@live.com>
2014-10-20 16:23:13 +0200

    Bugfix, exchange management, dates are wrongly interpreted when the server is run outside "en" cultures.

--- DayZTradeCenter.UI.Web/Global.asax.cs
+++ DayZTradeCenter.UI.Web/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DayZTradeCenter.DomainModel.Entities.Messages;
+using DayZTradeCenter.UI.Web.Helpers;
 
 namespace DayZTradeCenter.UI.Web
 {
@@ -14,6 +16,11 @@ namespace DayZTradeCenter.UI.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+#if DEBUG
+            // registers the custom model binder for "ExchangeDetails".
+            // This fixes the issue with dates like 10/20/2014 that are wrongly interpreted when run outside "en" culture.
+            ModelBinders.Binders[typeof (ExchangeDetails)] = new ExchangeDetailsModelBinder();
+#endif
         }
     }
 }
---
+++ DayZTradeCenter.UI.Web/Helpers/ExchangeDetailsModelBinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace DayZTradeCenter.UI.Web.Helpers
+{
+    /// <summary>
+    /// Fixes the issue with dates like 10/20/2014 that are wrongly interpreted when run outside "en" culture.
+    /// </summary>
+    public class ExchangeDetailsModelBinder : DefaultModelBinder
+    {
+        protected override void BindProperty(
+            ControllerContext controllerContext, 
+            ModelBindingContext bindingContext,
+            PropertyDescriptor propertyDescriptor)
+        {
+            if (propertyDescriptor.Name == "Time")
+            {
+                var value = controllerContext.HttpContext.Request.Form["Details.Time"];
+
+                var date =
+                    DateTime.ParseExact(value, "MM/dd/yyyy h:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+
+                propertyDescriptor.SetValue(bindingContext.Model, date);
+            }
+            else
+            {
+                base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+            }
+        }
+    }
+}
