d11d3c22d973457fa6cda0c89d672b6ba91528f8
righettig <righetti.giacomo@live.com>
2014-10-22 15:26:23 +0200

    Improved css bundling.

--- DayZTradeCenter.UI.Web/App_Start/BundleConfig.cs
+++ DayZTradeCenter.UI.Web/App_Start/BundleConfig.cs
@@ -7,68 +7,83 @@ namespace DayZTradeCenter.UI.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region Scripts
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/highcharts").Include(
-                        "~/Scripts/Highcharts-4.0.1/js/highcharts.js"));
+                "~/Scripts/Highcharts-4.0.1/js/highcharts.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/landing").Include(
-                        "~/Scripts/app/latesttrades.js",
-                        "~/Scripts/app/landing.js"));
+                "~/Scripts/app/latesttrades.js",
+                "~/Scripts/app/landing.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/index").Include(
-                        "~/Scripts/app/latesttrades.js",
-                        "~/Scripts/app/index.js"));
+                "~/Scripts/app/latesttrades.js",
+                "~/Scripts/app/index.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/layout").Include(
-                        "~/Scripts/app/_layout.js"));
+                "~/Scripts/app/_layout.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/trades_create").Include(
-                        "~/Scripts/knockout-{version}.js",
-                        "~/Scripts/app/CreateTradeViewModel.js",
-                        "~/Content/Selectize/js/standalone/selectize.js",
-                        "~/Scripts/ko_selectize.js"));
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/app/CreateTradeViewModel.js",
+                "~/Content/Selectize/js/standalone/selectize.js",
+                "~/Scripts/ko_selectize.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/trades_index").Include(
-                        "~/Content/Selectize/js/standalone/selectize.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/alertify.js",
-                        "~/Scripts/app/trades_index.js"));
+                "~/Content/Selectize/js/standalone/selectize.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js",
+                "~/Scripts/alertify.js",
+                "~/Scripts/app/trades_index.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/items_index").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/alertify.js",
-                        "~/Scripts/app/items_index.js"));
+                "~/Scripts/jquery.unobtrusive-ajax.js",
+                "~/Scripts/alertify.js",
+                "~/Scripts/app/items_index.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/inbox").Include(
-                        "~/Scripts/knockout-{version}.js",
-                        "~/Scripts/moment.js",
-                        "~/Scripts/alertify.js",
-                        "~/Scripts/app/InboxViewModel.js"));
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/moment.js",
+                "~/Scripts/alertify.js",
+                "~/Scripts/app/InboxViewModel.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/exchange").Include(
-                        "~/Scripts/moment.js",
-                        "~/Scripts/bootstrap-datetimepicker.js"));
+                "~/Scripts/moment.js",
+                "~/Scripts/bootstrap-datetimepicker.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/tradecompleted").Include(
-                        "~/Scripts/app/tradecompleted.js"));
+                "~/Scripts/app/tradecompleted.js"));
+
+            #endregion
+            
+            #region Styles
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap.css",
-                        "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/alertifyjs").Include(
+                "~/Content/alertifyjs/alertify.css",
+                "~/Content/alertifyjs/themes/default.css"));
+
+            bundles.Add(new StyleBundle("~/Content/pagedlist").Include(
+                "~/Content/PagedList.css"));
+
+            #endregion
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
